827ef35c8209c4ecd4fd83e17bfebc4dd8ea2b45
vallinarayanan123 <vallinarayanan@gmail.com>
2022-09-12 21:21:30 +0530

    RDET123

---
+++ src/encapsulation/org/Employeeinfo.java
@@ -0,0 +1,14 @@
+package encapsulation.org;
+
+public class Employeeinfo {
+private String Name;
+public void SetName(String empName) {
+	this.Name=empName;
+	
+}
+public String getName() {
+	return Name;
+	
+	
+}
+}
---
+++ src/encapsulation/org/Test.java
@@ -0,0 +1,13 @@
+package encapsulation.org;
+
+public class Test {
+	public static void main(String[] args) {
+		Employeeinfo E= new Employeeinfo();
+		E.SetName("karthi");
+		String name=E.getName();
+		System.out.println(name);
+		
+		
+	}
+
+}
